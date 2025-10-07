638c39d781551945f1f2afb8bccf552be2b0b02c
Md Aktar Ahmed <amdaktar86@gmail.com>
2025-10-07 18:18:20 +0600

    added search bar

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import useProducts from "../Hooks/useProduct";
 import ProductsCard from "../components/ProductsCard/ProductsCard";
 
 const Products = () => {
   const { products, loadign, error } = useProducts();
+  const [search, setSearch] = useState("");
+  const trim = search.trim().toLowerCase();
+  const searchedProducts = trim
+    ? products.filter((product) => product.name.toLowerCase().includes(trim))
+    : products;
+
+//   console.log(searchedProducts);
+
   return (
     <div className="py-10">
       <div className=" flex justify-between mb-2">
-        <h1 className="font-bold text-2xl">Products Fetures</h1>
-        <button className="btn">Search</button>
+        <h1 className="font-bold text-2xl">
+          Products Fetures{" "}
+          <span className="text-sm font-medium">
+            ({searchedProducts.length}) Products Available
+          </span>{" "}
+        </h1>
+
+        <label className="floating-label">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="mail@site.com"
+            className="input input-md"
+          />
+        </label>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {products.map((data) => (
+        {searchedProducts.map((data) => (
           <ProductsCard key={data.id} data={data}></ProductsCard>
         ))}
       </div>
